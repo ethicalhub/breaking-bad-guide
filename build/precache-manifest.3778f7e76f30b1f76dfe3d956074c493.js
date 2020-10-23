d652d453a166b8b4af7f2bc8ebcd8a3e6c21fa4c
ethicalhub <yasiransari.elith@gmail.com>
2020-10-23 08:13:08 +0530

    Build File Add

---
+++ build/precache-manifest.3778f7e76f30b1f76dfe3d956074c493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b42b482b6cc8541a8e1b490507c2fe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f34eeda2ed9e4f58df",
+    "url": "/static/js/2.76be6ef4.chunk.js"
+  },
+  {
+    "revision": "bc01c4d38a86ca86b3401a17116d292e",
+    "url": "/static/js/2.76be6ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280c61be96982f25b85f",
+    "url": "/static/js/main.d7cb8c24.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
